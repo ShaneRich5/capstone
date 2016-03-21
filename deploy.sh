001c68f5a06d23d5511335a56d8d71e99103cb2d
shane <shane.richards121@gmail.com>
2016-03-21 06:43:27 -0500

    updated the deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,27 +3,34 @@
 REMOTE=root@159.203.73.159
 REMOTE_APP=/root/capstone
 
-# ssh $REMOTE << EOF
+ssh $REMOTE << EOF
 
     if [ -d "$REMOTE_APP" ]; then
         git clone git@github.com:ShaneRich5/capstone.git
-        echo "cloned"
         cd capstone
-    else
+    elsehttps://www.youtube.com/watch?v=-ToxHXK2gDc&ebc=ANyPxKoCYbuyoSsfvcxqF1dptROW1y-tVIqXYU-ZID3rbifYV5joYj6QUddtI_auH8mXzu5rJLhToRyXzHENtVRe3LeQKUAKbA
         cd capstone
         echo "pulled"
-        pwd
         git pull origin master
     fi
-    pwd
-    echo "we made it"
 
 
     sbt stage || exit 1
-    rsync -va target/ $REMOTE:$REMOTE_APP/target;
+    #rsync -va target/ $REMOTE:$REMOTE_APP/target;
     #ssh $REMOTE "cd $REMOTE_APP"; ./stop.sh";
     #ssh $REMOTE "cd $REMOTE_APP; ./start.sh"
-    ./stop.sh
-    ./start.sh
+    #./stop.sh
+    #./start.sh
 
+    #stop
+    #test -f RUNNING_PID && kill `cat RUNNING_PID` && sleep 5;
+    #rm RUNNING_PID;
+
+    #start
+    #nohup ./target/universal/stage/bin/APPLICATION_SBT_NAME -Dhttp.port=9090 &
+
+    ./activator playGenerateSecret
+    ./activator dist
+    unzip target/universal/capstone-1.0-SNAPSHOT.zip
+    nohup ./capstone-1.0-SNAPSHOT/bin/capstone -Dhttp.port=9090
 EOF
