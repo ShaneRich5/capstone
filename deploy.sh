48a6414b0d3077d404d16298ea033bba1994be56
shane <shane.richards121@gmail.com>
2016-03-21 03:36:13 -0500

    modified deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,27 @@
 REMOTE=root@159.203.73.159
 REMOTE_APP=/root/capstone
 
-ssh $REMOTE <<EOF
+# ssh $REMOTE << EOF
+
     if [ -d "$REMOTE_APP" ]; then
         git clone git@github.com:ShaneRich5/capstone.git
+        echo "cloned"
         cd capstone
     else
         cd capstone
+        echo "pulled"
+        pwd
         git pull origin master
     fi
+    pwd
+    echo "we made it"
 
+
+    sbt stage || exit 1
+    rsync -va target/ $REMOTE:$REMOTE_APP/target;
+    #ssh $REMOTE "cd $REMOTE_APP"; ./stop.sh";
+    #ssh $REMOTE "cd $REMOTE_APP; ./start.sh"
+    ./stop.sh
+    ./start.sh
 
 EOF
-
-
