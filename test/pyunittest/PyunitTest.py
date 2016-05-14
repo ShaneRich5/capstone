a9a5d7cd60dd9fbf10ce3077d17daf63d295ef05
Tevin Anderson <Tevin Anderson>
2016-05-14 13:58:59 -0500

    Minor change to PyunitTest.py

--- test/pyunittest/PyunitTest.py
+++ test/pyunittest/PyunitTest.py
@@ -1,5 +1,36 @@
 import unittest
-import pyunittest.student.fooimport unittest
-import pyunittest.student.poo;
+import pyunittest.student.foo
+
+import pyunittest.student.poo
+
+import pyunittest.student.coo
+
+class Tester(unittest.TestCase):
+
+
+    mark = 5
+    name = "Assignment 1"
+    def setUp(self):
+        pass
+
+   def test0(self):
+       compareInt()
+       grade(1)
+
+
+   def test1(self):
+       compareInt()
+       grade(1)
+
+
+     # Method to compare integers
+   def compareInt(a, b):
+         self.assertEquals(a,b)
+
+     # Method to count grade
+   def grade(x):
+        grade.counter += x 
+
+
 if __name__ == '__main__':
     unittest.main()
