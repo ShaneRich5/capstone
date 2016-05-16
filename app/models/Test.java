13f2a53288ac6afbc855cc0a56f1349bd1917f78
Yerodin Richards <yero_din@hotmail.com>
2016-05-16 19:00:26 -0400

    Changes to models related to testing, updated assignment creation view

--- app/models/Test.java
+++ app/models/Test.java
@@ -1,15 +1,41 @@
 package models;
 
+import com.avaje.ebean.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.List;
 
 /**
  * Created by uhhh on 5/16/16.
  */
-public class Test {
+@Entity
+@Table(name = "tests")
+public class Test extends Model
+{
+    @Id
+    private int id;
 
-    public String testHeader;
+    private String testHeader;
 
     @OneToMany
-    public List<TestCase> testCases;
+    private List<TestCase> testCases;
+
+    public String getTestHeader() {
+        return testHeader;
+    }
+
+    public void setTestHeader(String testHeader) {
+        this.testHeader = testHeader;
+    }
+
+    public List<TestCase> getTestCases() {
+        return testCases;
+    }
+
+    public void setTestCases(List<TestCase> testCases) {
+        this.testCases = testCases;
+    }
 }
