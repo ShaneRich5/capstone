d49e247d5c402032268aaa417b5a701bf23de23f
Yerodin Richards <yero_din@hotmail.com>
2016-05-16 21:09:48 -0400

    Added Apache Commons IO dependency, created model for form and for Java Testing

---
+++ app/models/Forms/AssignmentForm.java
@@ -0,0 +1,11 @@
+package models.Forms;
+
+import play.data.validation.Constraints;
+
+/**
+ * Created by uhhh on 5/16/16.
+ */
+public class AssignmentForm {
+    @Constraints.Required
+    
+}
---
+++ app/models/JavaTest.java
@@ -0,0 +1,7 @@
+package models;
+
+/**
+ * Created by uhhh on 5/16/16.
+ */
+public class JavaTest {
+}
